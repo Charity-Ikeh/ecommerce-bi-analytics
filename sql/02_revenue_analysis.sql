938978952b792c9db0fdc0abb086a6b822e4445a
Charity-Ikeh <167613119+Charity-Ikeh@users.noreply.github.com>
2026-02-16 15:35:28 +0000

    Add revenue analysis SQL script
    This SQL script performs revenue analysis for an e-commerce project, including total revenue, monthly revenue, top products by revenue, and revenue by country.

---
+++ sql/02_revenue_analysis.sql
@@ -0,0 +1,43 @@
+/*
+========================================
+02 - REVENUE ANALYSIS (MySQL)
+E-Commerce BI Analytics Project
+Author: Charity Ikeh
+========================================
+*/
+
+-- 1. Total Revenue
+SELECT 
+    SUM(Quantity * UnitPrice) AS total_revenue
+FROM online_retail_clean;
+
+------------------------------------------------
+
+-- 2. Monthly Revenue
+SELECT 
+    DATE_FORMAT(InvoiceDate, '%Y-%m-01') AS invoice_month,
+    SUM(Quantity * UnitPrice) AS monthly_revenue
+FROM online_retail_clean
+GROUP BY invoice_month
+ORDER BY invoice_month;
+
+------------------------------------------------
+
+-- 3. Top 10 Products by Revenue
+SELECT 
+    Description,
+    SUM(Quantity * UnitPrice) AS product_revenue
+FROM online_retail_clean
+GROUP BY Description
+ORDER BY product_revenue DESC
+LIMIT 10;
+
+------------------------------------------------
+
+-- 4. Revenue by Country
+SELECT 
+    Country,
+    SUM(Quantity * UnitPrice) AS country_revenue
+FROM online_retail_clean
+GROUP BY Country
+ORDER BY country_revenue DESC;

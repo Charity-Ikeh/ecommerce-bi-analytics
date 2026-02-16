6bdd229457f01a4d714d8c11ea174bf5896f95ae
Charity-Ikeh <167613119+Charity-Ikeh@users.noreply.github.com>
2026-02-16 17:30:06 +0000

    Update SQL comments and queries for SQLite compatibility

--- sql/03_customer_analysis_sqlite.sql
+++ sql/03_customer_analysis_sqlite.sql
@@ -1,25 +1,20 @@
 /*
 ========================================
-03 - CUSTOMER ANALYSIS (MySQL)
+03 - CUSTOMER ANALYSIS (SQLite)
 E-Commerce BI Analytics Project
 Author: Charity Ikeh
 ========================================
-
-Purpose:
-- Calculate total customers
-- Identify top customers by revenue
-- Calculate average revenue per customer
 */
 
 -- 1. Total Unique Customers
-SELECT 
+SELECT
     COUNT(DISTINCT CustomerID) AS total_customers
 FROM online_retail_clean;
 
 --------------------------------------------------
 
 -- 2. Top 10 Customers by Revenue
-SELECT 
+SELECT
     CustomerID,
     SUM(Quantity * UnitPrice) AS customer_revenue
 FROM online_retail_clean
@@ -30,7 +25,6 @@ LIMIT 10;
 --------------------------------------------------
 
 -- 3. Average Revenue Per Customer
-SELECT 
-    SUM(Quantity * UnitPrice) / COUNT(DISTINCT CustomerID) 
-    AS avg_revenue_per_customer
+SELECT
+    SUM(Quantity * UnitPrice) * 1.0 / COUNT(DISTINCT CustomerID) AS avg_revenue_per_customer
 FROM online_retail_clean;
